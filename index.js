6fee2bf89149f4215f219964f17ac64ca9600506
beasta <@>
2022-06-22 11:05:08 -0700

    working module to scrape main etherscan lable

---
+++ index.js
@@ -0,0 +1,43 @@
+const superagent = require('superagent');
+
+// Just checks plausibility
+const isPlausibleAddress = (address) => (/^(0x){1}[0-9a-fA-F]{40}$/i.test(address));
+
+module.exports = function EtherscanLabels() {
+  if (!(this instanceof EtherscanLabels)) {
+    return new EtherscanLabels();
+  }
+  this.grabLabel = (body) => {
+    const fullLabelRegex = /\(viewable by anyone\)'>(.*?)</;
+    const fullLabel = body.match(fullLabelRegex)[1];
+    if (fullLabel === null) { // checking for errors if the div wasn't matched
+      return 'There has been an error processing this address (span was not matched in body)';
+    }
+    if (fullLabel) {
+      return { fullLabel };
+    }
+    return 'There has been an error processing this address';
+  };
+
+  this.requestEtherscan = (address, callback) => {
+    if (!address || !isPlausibleAddress(address)) {
+      callback('Must enter a valid address');
+      return;
+    }
+    superagent
+      .get(`https://etherscan.io/address/${address}`)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) {
+          callback(err);
+        } else {
+          const labelsObject = this.grabLabel(res.res.text);
+          if (typeof labelsObject === 'string') {
+            callback(labelsObject); // return error
+          } else {
+            callback(null, this.grabLabel(res.res.text));
+          }
+        }
+      });
+  };
+};
